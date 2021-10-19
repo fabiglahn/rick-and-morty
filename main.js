93daf024e01a0a3b6d3031ddef3e474087209963
“[fabiglahn] <[fabian-glahn@hotmail.de]>
2021-10-19 12:28:34 +0200

    further JS

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
 import "./style.css";
+import { createElement } from "./lib/elements.js";
+import { createCardComponent } from "./lib/card";
 
-document.querySelector("#app").innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+function renderApp() {
+  const appElement = document.querySelector("#app");
+
+  const headerElement = createElement("header", { className: "header" }, [
+    createElement("h1", {
+      textContent: "THE RICK & MORTY API",
+    }),
+  ]);
+
+  const mainElement = createElement("main", { className: "main" }, [
+    createElement("p", { textContent: "Still developing..." }),
+  ]);
+
+  const cardComponent = createCardComponent();
+
+  const footerElement = createElement("footer", { className: "footer" }, [
+    createElement("p", { textContent: "Footer content" }),
+  ]);
+
+  appElement.append(headerElement, mainElement, cardComponent, footerElement);
+}
+
+renderApp();
